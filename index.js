de43ed9117380a2f1c4e7729ae5d55f81a54d19c
dattapadre <dattapadre7249@gmail.com>
2025-11-10 11:15:48 +0530

    database

--- index.js
+++ index.js
@@ -1,42 +1,113 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const UserModel = require('./models/User');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const UserModel = require("./models/User");
+const multer = require("multer");
+const path = require("path");
 
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-mongoose.connect('mongodb://localhost:27017/AuthDB');
+mongoose.connect("mongodb+srv://dattapadre:dattapadre@cluster0.1cpqqak.mongodb.net/?appName=Cluster0");
 
 
-app.post('/api/signup', async (req, res) => {
-  console.log(req.body);
+app.get("/", function (req, res) {
+  res.send("Work");
+});
+
+app.post("/api/signup", async (req, res) => {
+  console.log("Incoming Data:", req.body);
 
   try {
     const { username, email, password } = req.body;
 
-    const existingUser = await UserModel.findOne({ email });
-    if (existingUser) {
-      return res.status(400).json({ message: 'Email already registered' });
+    if (!username || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
     }
 
-    const user = new UserModel({
-      username,
-      email,
-      password,
-    });
+    const existingUser = await UserModel.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "Email already registered" });
+    }
 
+    const user = new UserModel({ username, email, password });
     await user.save();
 
-    res.status(201).json({ message: 'User registered successfully', user });
+    res.status(201).json({ message: "User registered successfully", user });
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Server Error', error });
+    console.error(" Server Error:", error);
+    res.status(500).json({ message: "Server Error", error });
   }
 });
 
+app.post("/api/login", async function (req, res) {
+  console.log(req.body);
+  try {
+    const { email, password } = req.body;
+
+    const user = await UserModel.findOne({ email, password });
+
+    if (user) {
+      res
+        .status(200)
+        .json({ success: true, message: "Login successful", user });
+    } else {
+      res.json({ success: false, message: "Invalid email or password" });
+    }
+  } catch (err) {
+    console.error("Login Error:", err);
+    res
+      .status(500)
+      .json({ success: false, message: "Server Error", error: err.message });
+  }
+});
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage });
+
+app.post("/api/upload", upload.single("image"), async (req, res) => {
+  const userId = req.body.user_id;
+
+  const user = await UserModel.findByIdAndUpdate(
+    userId,
+    { profile: req.file.filename },
+    { new: true }
+  );
+
+  res.json({
+    success: true,
+    message: "Image uploaded successfully!",
+    filePath: `uploads/${req.file.filename}`,
+  });
+});
+
+app.post("/api/user_info", async function (req, res) {
+
+  console.log(req.body)
+  try {
+    var user_id = req.body.user_id;
+    var user_data = await UserModel.findById(user_id);
+    
+    console.log(user_data)
+
+    res.status(200).json({ success: true, message: "Login successful", user_data });
+
+  } catch (err) {
+    res
+      .status(500).json({ success: false, message: "Server Error", error: err.message });
+  }
+});
 
 app.listen(1000);
--- models/User.js
+++ models/User.js
@@ -4,7 +4,12 @@ const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, trim: true },
   email: { type: String, required: true, unique: true, lowercase: true },
   password: { type: String, required: true },
-  profile: { type: String, default: "" }
+  profile: { type: String, default: "" },
+  fullname: { type: String},
+  mobile: { type: String},
+  gender: { type: String},
+  Payment:{type:String,default:""},
+  status :{type:String,default:'active'}
 });
 
 module.exports = mongoose.model("User", UserSchema);
